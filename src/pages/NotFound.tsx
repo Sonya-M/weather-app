6e2fe5df2ef62610343c6165294c332be8a8f4f7
Sonja <sonja.musicki@gmail.com>
2021-11-22 11:16:43 +0100

    minor style improvement

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,21 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const CenteredLink = styled.span`
+  font-size: 1.2rem;
+  text-align: center;
+  & a {
+    text-decoration-color: orangered;
+  }
+`;
 
 export default function NotFound() {
   return (
     <>
       <h3>Sorry, the page you are looking for does not exist</h3>
-      <Link to="/">Home</Link>
+      <CenteredLink>
+        <Link to="/">Home</Link>
+      </CenteredLink>
     </>
   );
 }

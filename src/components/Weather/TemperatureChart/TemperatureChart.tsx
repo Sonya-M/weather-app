049e3ff9da2bff21395ea24eb5defce470c3e2f7
Sonja Musicki <sonja.musicki@gmail.com>
2021-11-29 13:45:08 +0100

    removed unnecessary curly braces in template literals and changed component names

---
+++ src/components/Weather/TemperatureChart/TemperatureChart.tsx
@@ -0,0 +1,63 @@
+import * as getters from "../../../utils/getters";
+import { ForecastData } from "../../../models/ForecastData";
+import TemperatureChartBar from "./TemperatureChartBar";
+import styled from "styled-components";
+
+const CHART_HEIGHT = 300; // 300 px, set it once
+const PADDING = 16;
+
+const TempChart = styled.div`
+  margin: auto;
+  padding: ${PADDING}px;
+  border-radius: 8px;
+  background-color: #f8dfff;
+  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  width: max-content;
+  max-width: 95vw;
+  overflow-x: auto;
+
+  display: flex;
+  justify-content: flex-start;
+  gap: 0.2rem;
+
+  height: ${CHART_HEIGHT + PADDING * 2}px; /* fixed width for easy calc */
+
+  min-height: fit-content;
+
+  @media screen and (min-width: 400px) {
+    gap: 0.4rem;
+  }
+`;
+
+const TemperatureChart: React.FC<{ data: ForecastData }> = (props) => {
+  const dailyF = getters.getDailyForecast(props.data);
+  const forecast = dailyF.map((f) => ({ min: f.min, max: f.max, day: f.day }));
+  const weeklyMax = Math.max(...dailyF.map((f) => f.max));
+  const weeklyMin = Math.min(...dailyF.map((f) => f.min));
+  // note that data is in Kelvins for easier calculations :)
+  const tempRange = weeklyMax - weeklyMin;
+
+  return (
+    <>
+      <h3>Temperature</h3>
+      <TempChart>
+        {forecast.map((f, i) => (
+          <TemperatureChartBar
+            tempForecast={f}
+            key={"" + i}
+            weeklyMax={weeklyMax}
+            weeklyMin={weeklyMin}
+            range={tempRange}
+            chartHeight={CHART_HEIGHT}
+          />
+        ))}
+      </TempChart>
+    </>
+  );
+};
+
+export default TemperatureChart;
+
+// TODO: make the chart more general (reusable) - that's why keep props, just
+// make the more general
---
+++ src/components/Weather/TemperatureChart/TemperatureChartBar.tsx
@@ -0,0 +1,96 @@
+import { KelvinToCelsius } from "../../../utils/converters";
+import styled from "styled-components";
+
+const TempBar = styled.div`
+  height: 100%;
+  width: 2.3rem;
+  max-width: 10vw;
+
+  & img {
+    max-width: 100%;
+  }
+
+  @media screen and(min-width: 400px) {
+    width: 3rem;
+  }
+`;
+
+const ChartBarInner = styled.div`
+  height: 100%;
+  width: 100%;
+  border-radius: 0.3rem;
+  /* overflow: hidden; */
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+`;
+
+const ChartBarFillMax = styled.div`
+  background-color: #eff301;
+  width: 100%;
+  position: relative;
+  bottom: 0;
+`;
+
+const ChartBarFillMin = styled.div`
+  background-color: #008cff;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+`;
+
+const ChartBarLabel = styled.div`
+  font-weight: bold;
+  font-size: 0.7rem;
+  text-align: center;
+  z-index: 2;
+`;
+
+const TemperatureChartBar: React.FC<{
+  tempForecast: {
+    min: number;
+    max: number;
+    day: string;
+  };
+  range: number;
+  weeklyMin: number;
+  weeklyMax: number;
+  chartHeight: number;
+}> = (props) => {
+  let maxBarFillHeight = "100%";
+  let minBarFillHeight = "100%";
+
+  if (props.range > 0) {
+    // leave some space for the labels, like 10%:
+    const oneDegreeHeight = Math.round(
+      (0.9 * props.chartHeight) / (props.range + 1)
+    );
+    maxBarFillHeight =
+      (props.tempForecast.max - props.weeklyMin + 1) * oneDegreeHeight + "px";
+    minBarFillHeight =
+      (props.tempForecast.min - props.weeklyMin + 1) * oneDegreeHeight + "px";
+    // console.log(`1 deg height: ${oneDegreeHeight};
+    // maxBarFillHeight: ${maxBarFillHeight};
+    // minBarFillHeight: ${minBarFillHeight}`);
+  }
+  return (
+    <TempBar>
+      <ChartBarInner>
+        <ChartBarLabel>{`${KelvinToCelsius(
+          props.tempForecast.max
+        )}°`}</ChartBarLabel>
+
+        <ChartBarFillMax style={{ height: maxBarFillHeight }}>
+          <ChartBarFillMin style={{ height: minBarFillHeight }} />
+        </ChartBarFillMax>
+
+        <ChartBarLabel>{`${KelvinToCelsius(
+          props.tempForecast.min
+        )}°`}</ChartBarLabel>
+      </ChartBarInner>
+      <ChartBarLabel>{props.tempForecast.day}</ChartBarLabel>
+    </TempBar>
+  );
+};
+
+export default TemperatureChartBar;
